2f898aac0a327faa3db81e42431555eb3a718431
pajid9763 <pajid9763@wrdsb.ca>
2020-10-07 19:48:41 +0000

    finished average grade program

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that finds your average grade
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // create the array for the grades to be stored
+    double grades[] = new double[5];
+
+    // get grades from the user and store them in an array
+    for (int i = 0; i < grades.length; i++){
+      System.out.println("Please enter grade " + (i + 1));
+      grades[i] = input.nextDouble();
+    }
+
+    // create the sum for the running total of the grades 
+    double sum = 0;
+
+    // add up all of the grades into sum 
+    for (int i = 0; i < grades.length; i++){
+      sum = sum + grades[i];
+    } 
     
+    // determine the average grade and let the user know
+    double average = sum/grades.length;
+    System.out.println("Your average grade is " + average);
   }
 }
